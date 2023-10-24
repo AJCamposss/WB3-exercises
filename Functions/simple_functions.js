bb3d0a000e8fd2020a648dab554af0b7e2cf38dd
AJCamposss <adrian.campos869@gmail.com>
2023-10-24 13:33:23 -0700

    Completed exercise

---
+++ Functions/simple_functions.js
@@ -0,0 +1,18 @@
+function favoriteThings() {
+    console.log("Adrian")
+    console.log("Scream")
+    console.log("Larry June")
+}
+
+function whyImHere() {
+    console.log("The reason I am here is to step my foot into the tech world")
+}
+
+function favoritePlace() {
+    console.log("My favorite place to visit is Mexico because the food just tastes so much better over there.")
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
+console.log("end of file")
